974866dd35a604340166fffeb495c4f2ec0cbfc3
Tomas <jtml.mass@gmail.com>
2020-05-25 03:11:19 -0400

    Agregar filtrado por Matricula

--- src/main/java/com/pucmm/crud_springboot/repositorios/EstudianteRepository.java
+++ src/main/java/com/pucmm/crud_springboot/repositorios/EstudianteRepository.java
@@ -1,5 +1,9 @@
 package com.pucmm.crud_springboot.repositorios;
 import com.pucmm.crud_springboot.entidades.Estudiante;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
 public interface EstudianteRepository extends JpaRepository<Estudiante, Integer>{
+    Estudiante findByMatricula(int matricula);
 }
